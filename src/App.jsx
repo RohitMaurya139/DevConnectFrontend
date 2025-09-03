7f8eebafae9599465254af7bf22e742a21c5ef44
RohitMaurya139 <rohit139maurya@gmail.com>
2025-09-03 17:10:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl text-red-500 font-bold m-3 p-3'>Hello, Developer Welcome to DevConnect</h1>
+   </>
+  )
+}
+
+export default App
